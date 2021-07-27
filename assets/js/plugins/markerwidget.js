12ebb6102e8edc1ec35cc831cee836aeed903474
LeeWonWoo1 <plmplmdnjsdn@naver.com>
2021-07-28 03:11:18 +0900

    markerwidget.js

---
+++ assets/js/plugins/markerwidget.js
@@ -0,0 +1,13 @@
+(function (d, h, m) {
+  var js, fjs = d.getElementsByTagName(h)[0];
+  if (d.getElementById(m)) { return; }
+  js = d.createElement(h); js.id = m;
+  js.onload = function () {
+    window.makerWidgetComInit({
+      position: "left",
+      widget: "tuyl32c3ess7do0e-m2sq4fnkgzyoella-9hioakhimlb29ayr"
+    })
+  };
+  js.src = "https://makerwidget.com/js/embed.js";
+  fjs.parentNode.insertBefore(js, fjs)
+}(document, "script", "dhm"))

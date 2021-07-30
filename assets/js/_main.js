d55df9117acb1061f0e5e0328907bb352cc5c434
LeeWonWoo1 <plmplmdnjsdn@naver.com>
2021-07-30 19:13:47 +0900

    post jckp5

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -2,12 +2,12 @@
    jQuery plugin settings and other scripts
    ========================================================================== */
 
-$(document).ready(function() {
+$(document).ready(function () {
   // FitVids init
   $("#main").fitVids();
 
   // Sticky sidebar
-  var stickySideBar = function() {
+  var stickySideBar = function () {
     var show =
       $(".author__urls-wrapper button").length === 0
         ? $(window).width() > 1024 // width should match $large Sass variable
@@ -23,18 +23,18 @@ $(document).ready(function() {
 
   stickySideBar();
 
-  $(window).resize(function() {
+  $(window).resize(function () {
     stickySideBar();
   });
 
   // Follow menu drop down
-  $(".author__urls-wrapper button").on("click", function() {
+  $(".author__urls-wrapper button").on("click", function () {
     $(".author__urls").toggleClass("is--visible");
     $(".author__urls-wrapper button").toggleClass("open");
   });
 
   // Close search screen with Esc key
-  $(document).keyup(function(e) {
+  $(document).keyup(function (e) {
     if (e.keyCode === 27) {
       if ($(".initial-content").hasClass("is--hidden")) {
         $(".search-content").toggleClass("is--visible");
@@ -44,11 +44,11 @@ $(document).ready(function() {
   });
 
   // Search toggle
-  $(".search__toggle").on("click", function() {
+  $(".search__toggle").on("click", function () {
     $(".search-content").toggleClass("is--visible");
     $(".initial-content").toggleClass("is--hidden");
     // set focus on input
-    setTimeout(function() {
+    setTimeout(function () {
       $(".search-content input").focus();
     }, 400);
   });
@@ -62,7 +62,7 @@ $(document).ready(function() {
   });
 
   // Gumshoe scroll spy init
-  if($("nav.toc").length > 0) {
+  if ($("nav.toc").length > 0) {
     var spy = new Gumshoe("nav.toc a", {
       // Active classes
       navClass: "active", // applied to the nav list item
@@ -109,7 +109,7 @@ $(document).ready(function() {
     // make it unique to apply your CSS animations just to this exact popup
     mainClass: "mfp-zoom-in",
     callbacks: {
-      beforeOpen: function() {
+      beforeOpen: function () {
         // just a hack that adds mfp-anim class to markup
         this.st.image.markup = this.st.image.markup.replace(
           "mfp-figure",
@@ -122,7 +122,7 @@ $(document).ready(function() {
   });
 
   // Add anchors for headings
-  $('.page__content').find('h1, h2, h3, h4, h5, h6').each(function() {
+  $('.page__content').find('h1, h2, h3, h4, h5, h6').each(function () {
     var id = $(this).attr('id');
     if (id) {
       var anchor = document.createElement("a");
